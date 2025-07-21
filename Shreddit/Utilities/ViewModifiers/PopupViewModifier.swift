8905d11f5069f07077d1b2158956808aa3f3c23c
Mihnea <m@mihneaparvanu.ro>
2025-07-21 16:02:33 +0300

    implemengted view methods for popup

--- Shreddit/Utilities/ViewModifiers/PopupViewModifier.swift
+++ Shreddit/Utilities/ViewModifiers/PopupViewModifier.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct BooleanPopupViewModifier<PopupContent: View>: ViewModifier {
 	//MARK: Dependencies
 	@Binding var isPresented: Bool
-	@ViewBuilder let popupContent: () ->  PopupContent
+	@ViewBuilder let popupContent: () -> PopupContent
 	
 	func body(content: Content) -> some View {
 		if isPresented {
@@ -27,6 +27,7 @@ struct ItemPopupViewModifier<Item: Identifiable, PopupContent: View>: ViewModifi
 	//MARK: Dependencies
 	@Binding var item: Item?
 	@ViewBuilder let popupContent: (Item) -> PopupContent
+	
 	func body(content: Content) -> some View {
 		if let item = item {
 			PopupView(mainContent: {content}) {
@@ -39,6 +40,18 @@ struct ItemPopupViewModifier<Item: Identifiable, PopupContent: View>: ViewModifi
 }
 
 extension View {
+	func popup (isPresented: Binding<Bool>, @ViewBuilder popupContent: @escaping () -> some View) -> some View {
+		self.modifier(
+			BooleanPopupViewModifier(
+				isPresented: isPresented,
+				popupContent: popupContent
+			)
+		)
+	}
+	
+	func popup<Item: Identifiable>(item: Binding<Item?>, @ViewBuilder popupContent: @escaping (Item) -> some View) -> some View {
+		self.modifier(ItemPopupViewModifier(item: item, popupContent: popupContent))
+	}
 }
 
 #Preview {
