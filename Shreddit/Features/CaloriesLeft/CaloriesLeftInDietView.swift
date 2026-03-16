1802f47e38c87aa841e7dbc0bba90ba55c82528d
Mihnea <m@mihneaparvanu.ro>
2026-03-16 23:55:18 +0200

    Starting work on Calories left in diet feature

---
+++ Shreddit/Features/CaloriesLeft/CaloriesLeftInDietView.swift
@@ -0,0 +1,14 @@
+//
+//  CaloriesLeftInDietView.swift
+//  Shreddit
+//
+//  Created by Mihnea Nicolae Pârvanu on 3/16/26.
+//
+
+import SwiftUI
+
+struct CaloriesLeftInDietView: View {
+	var body: some View {
+	}
+}
+ 
